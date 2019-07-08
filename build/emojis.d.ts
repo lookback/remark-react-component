7724e3ae528203b58345ff2e6d98154ad116074f
Johan Brook <johan@lookback.io>
2019-07-08 10:46:29 +0200

    Fix build

--- build/emojis.d.ts
+++
@@ -1,2 +0,0 @@
-export { default as emojis } from './tokenizers/emojis';
-export { traverseEmojis as addEmojiClasses, } from './transformers/emoji-transformer';
--- build/index.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="react" />
-export declare const createComponentFromProcessor: (processor: import("unified").Processor, options?: import("./mdast-to-react").Options) => import("react").FunctionComponent<import("./mdast-to-react").Props>;
